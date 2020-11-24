b96f4c5197e9dd504767b2941927d6a0574b37a1
donall1 <73856624+donall1@users.noreply.github.com>
2020-11-24 08:17:38 -0800

    Added Pokemon Array
    Added three types of pokemon to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = "steak";
-document.write(favoriteFood);
+let = 'pokemonList';
+let pokemonArray = [
+  { name: 'bulbasaur', height: 0.7, type: 'grass', 'poison'},
+  { name: 'ekans', height: 2, type: 'poison'},
+  { name: 'fearow', height: 1.2, type: 'flying', 'nomral'},
+];
