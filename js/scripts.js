ff95b6e6b1ae093bc16fe6ac7cc631ae5933cd2b
donall1 <73856624+donall1@users.noreply.github.com>
2020-11-24 20:08:37 -0800

    Added Ul to Html

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,21 @@ let pokemonArray = [
   { name: 'ekans', height: 2, type: 'poison'},
   { name: 'fearow', height: 1.2, type: ['flying', 'nomral']},
 ];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 2) {
+    document.write(
+      pokemonList[i].name + "" + " (height: " + pokemonList[i].height + ") "
+    );
+  } else {
+    document.write(
+      pokemonList[i].name +
+        "" +
+        " (height: " +
+        pokemonList[i].height +
+        ")" +
+        " -Wow, that is big! "
+    );
+  }
+}
