bbb98a5d557018923fabb03e28379a97100f7eff
donall1 <73856624+donall1@users.noreply.github.com>
2020-11-24 20:13:27 -0800

    Update scripts.js
    added brackets to ekins

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let = 'pokemonList';
 let pokemonArray = [
   { name: 'bulbasaur', height: 0.7, type: ['grass', 'poison']},
-  { name: 'ekans', height: 2, type: 'poison'},
+  { name: 'ekans', height: 2, type: ['poison']},
   { name: 'fearow', height: 1.2, type: ['flying', 'nomral']},
 ];
 
