aab8f6607dcc7fb192015b886ab90454d88d4a65
donall1 <73856624+donall1@users.noreply.github.com>
2020-11-24 19:40:51 -0800

    Update scripts.js
    added brackets

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let = 'pokemonList';
 let pokemonArray = [
-  { name: 'bulbasaur', height: 0.7, type: 'grass', 'poison'},
+  { name: 'bulbasaur', height: 0.7, type: ['grass', 'poison']},
   { name: 'ekans', height: 2, type: 'poison'},
-  { name: 'fearow', height: 1.2, type: 'flying', 'nomral'},
+  { name: 'fearow', height: 1.2, type: ['flying', 'nomral']},
 ];
