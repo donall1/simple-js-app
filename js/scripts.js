9e5da7c6a2977a4b8b7fad1f81d47e4c0079d51e
donall1 <73856624+donall1@users.noreply.github.com>
2020-11-28 11:41:35 -0800

    Changed for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
-let = 'pokemonList';
-let pokemonArray = [
+let pokemonList = [
   { name: 'bulbasaur', height: 0.7, type: ['grass', 'poison']},
   { name: 'ekans', height: 2, type: ['poison']},
   { name: 'fearow', height: 1.2, type: ['flying', 'nomral']},
 ];
 
 
+
+
 for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height < 1) {
+  if (pokemonList[i].height <= 1) {
     document.write(
-      pokemonlist[i].name + pokemonList[i].height
+      pokemonList[i].name + "" + " (height: " + pokemonList[i].height + ") "
     );
   } else {
-    document.write
-    pokemonList[i].name + "" + " (height: " + pokemon.List[i].height + ")" + "-wow, that is big! "
+    document.write(
+      pokemonList[i].name + "" + " (height: " + pokemonList[i].height +  ")" + " -Wow, that is big! ");
   }
 }
